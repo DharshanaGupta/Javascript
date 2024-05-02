ad88d90d85bd44a71f6253681e1709d2a8099e0e
DharshanaGupta <dharshana.sf4609@gmail.com>
2024-05-03 00:46:08 +0530

    Adding file of Login form Validation

--- LoginFormValidation/js/validate.js
+++ LoginFormValidation/js/validate.js
@@ -1,30 +1,157 @@
 //form validation
 
-var mailid=document.getElementById("email");
-var number=document.getElementById("phone");
-var password=document.getElementById("pwd");
+
+
+
 var gender=document.getElementById("gender");
-var address=document.getElementById("address");
+
 var dob=document.getElementById("dob");
 var time=document.getElementById("time");
-var language=document.getElementById("lang");
+
 var interest=document.getElementById("interest");
-// function validate()
-// {
-//    validateName();
-// }
+function validate()
+{
+   
+    
+   validateName();
+   validateEmail();
+   validateMobile();
+   validatePassword();
+   validateCheckbox();
+   validateAddress();
+   Display();
+   
+}
+function Display()
+{
+    if (validateName() && validateEmail() && validateMobile() && validatePassword() && validateCheckbox() && validateAddress()) 
+    {
+        DisplayDetails();
+    }
+    
+}
 
 function validateName()
 {
     var name=document.getElementById("name");
     if(name.value.trim()=="")
     {
-        alert("hi");
+        alert("Name must be filled");
         name.style.border="solid 2px red";
-        document.getElementById("lbuser").style.visibility="visible";
+        //document.getElementById("lbuser").style.visibility="visible";
         return false;
     }
+    else{
+        name.style.border="solid 2px green";
+        return true;
+    }
    
+}
+
+function validateMobile()
+{
+    var number=document.getElementById("phone").value;
+    regex=/^[7-9]\d{9}$/;
+    if(regex.test(number))
+    {
+        document.getElementById("phone").style.border="solid 2px green";
+        //document.getElementById("phone").innerHTML="valid";
+        return true;
+    }
+    else{
+        alert("invalid number");
+        document.getElementById("phone").style.border="solid 2px red";
+        return false;
+        //document.getElementById("lbnumber").style.visibility="visible";
+    }
+}
+
+function validateEmail()
+{
+    var mailid=document.getElementById("email");
+    regex=/^([a-zA-z0-9\.-]+)@([a-zA-Z0-9-]+).([a-zA-Z]{2,20})(.[a-zA-Z]{2,20})?$/ //?-denotes optional
+    if(regex.test(email.value))
+    {
+        mailid.style.border="solid 2px green";
+        return true;
+    }
+    else{
+        alert("invalid mailid");
+        mailid.style.border="solid 2px red";
+        return false;
+        //document.getElementById("lbmail").style.visibility="visible";
+    }
+}
+
+function validatePassword()
+{
+    var password=document.getElementById("pwd");
+    regex=/^[a-zA-Z0-9 ! @ # $ % ^ & * ( ) - + ' " , / \/]+$/;
+    if(regex.test(password.value))
+    {
+        password.style.border="solid 2px green";
+        return true;
+    }
+    else{
+        alert("invalid Password");
+        //document.getElementById("password").style.border="solid 2px red";
+        password.style.border="solid 2px red";
+        return false;
+        //document.getElementById("lbpassword").style.visibility="visible";
+    }
+    // if(document.getElementById("password").value.length<8)
+    // {
+    //     document.getElementById("password").style.border="solid 2px red";
+    //     document.getElementById("lbpasswordlen").style.visibility="visible";
+    // }
+}
+
+function validateAddress()
+{
+    var address=document.getElementById("address");
+    if(address.value.trim()=="")
+    {
+        alert("Please fill out the address");
+        address.style.border="solid 2px red";
+        return false;
+
+    }
+    else{
+        address.style.border="solid 2px green";
+        return true;
+    }
+}
+
+function validateCheckbox()
+{
+    var lang1=document.getElementById("lang1");
+    var lang2=document.getElementById("lang2");
+    var lang3=document.getElementById("lang3");
+    var string="";
+    var checked=[];
+    if(lang1.checked==true)
+    {
+        string=document.getElementById("lang1").value;
+        checked.push(string);
+    }
+    if(lang2.checked==true)
+    {
+        string=document.getElementById("lang2").value;
+        checked.push(string);
+    }
+    if(lang3.checked==true)
+    {
+        string=document.getElementById("lang3").value;
+        checked.push(string);
+    }
+    if(checked.length==0)
+    {
+        alert("please select the language");
+        return false;
+    }
+    else{
+        return true;
+    }
 }
 
 // document.getElementById("btn1").addEventListener("click", function()
